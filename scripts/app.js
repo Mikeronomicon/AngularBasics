1584743ccd33cb4b0fea08e66b7c4db654feab5e
Michael <Mikeronomicon@gmail.com>
2015-11-25 12:32:36 -0800

    added ability to edit fields, trying to get css and angular to notice each other.

--- scripts/app.js
+++ scripts/app.js
@@ -3,4 +3,16 @@ angular.module('todoListApp', [])
     $scope.helloWorld = function() {
       console.log("it worked!");
     };
+
+    $scope.todos = [{
+      "name": "clean the house"
+    }, {
+      "name": "water the butts"
+    }, {
+      "name": "eat like a sarrlak"
+    }, {
+      "name": "poop"
+    }, {
+      "name": "drink all the beers"
+    }]
   });
