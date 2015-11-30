5801ce7aa022f105055d23d4961cb904caf65f44
Michael <Mikeronomicon@gmail.com>
2015-11-30 14:22:45 -0800

    seem to have broken something. Going to test to see what broke.

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,25 @@
 angular.module('todoListApp', [])
   .controller('mainCtrl', function($scope, dataService) {
-    $scope.learningNgChange = function() {
-      console.log("an input changed");
-    };
-    $scope.helloConsole = dataService.helloConsole;
+    $scope.addTodo = function() {
+      var todo = {
+        name: "This is a new todo."
+      };
+      $scope.todos.push(todo);
+    }
+  });
 
-    dataService.getTodos(function(response) {
-      console.log(response.data);
-      $scope.todos = response.data;
-    });
-  })
+dataService.getTodos(function(response) {
+  console.log(response.data);
+  $scope.todos = response.data;
+});
+
+$scope.deleteTodo = function(todo, $index) {
+  dataService.deleteTodo(todo);
+  $scope.todos.splice($index, 1);
+};
+$scope.saveTodo = function(todo) {
+  dataService.saveTodo(todo)
+}
 
 .service('dataService', function($http) {
   this.helloConsole = function() {
@@ -17,9 +27,13 @@ angular.module('todoListApp', [])
   };
 
   this.getTodos = function(callback) {
-    $http.get('mock/todos.json')
-      .then(function(callback) {
-
-      })
+    $http.get('/mock/todos.json')
+      .then(callback)
+  };
+  this.deleteTodo = function(todo) {
+    console.log("the " + todo.name + "has been deleted!")
+  };
+  this.saveTodo = function(todo) {
+    console.log("the " + todo.name + "has been saved!")
   }
 });
