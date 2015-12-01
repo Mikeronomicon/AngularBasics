7632ba81157783a273a2407cee0d906746531523
Michael <Mikeronomicon@gmail.com>
2015-12-01 15:22:43 -0800

    app scaffolding complete

---
+++ scripts/services/data.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('todoListApp')
+  .service('dataService', function($http) {
+    this.helloConsole = function() {
+      console.log("you are being serviced")
+    };
+
+    this.getTodos = function(callback) {
+      $http.get('/mock/todos.json')
+        .then(callback)
+    };
+    this.deleteTodo = function(todo) {
+      console.log("the " + todo.name + "has been deleted!")
+    };
+    this.saveTodo = function(todo) {
+      console.log("the " + todo.name + "has been saved!")
+    }
+  });
